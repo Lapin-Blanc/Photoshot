5abc9f732f83be941001e67732830bac2b6ae0de
Lapin-Blanc <github@lapin-blanc.com>
2013-11-09 13:42:41 +0100

    First Django config

--- Photoshot/Photoshot/settings.py
+++ Photoshot/Photoshot/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Fabien\\workspace\\Photoshot\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\\Users\\Fabien\\git\\Photoshot\\Photoshot\\sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -121,9 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'pictures',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
